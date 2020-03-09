a1ac01303a7e9821b4f5ae651fe485f5f2d1c5d7
EduardaAChagas <adraudech@gmail.com>
2020-03-09 10:38:32 -0300

    Hello World!

---
+++ 01 - Variáveis/q01.py
@@ -0,0 +1,10 @@
+#Questão 1:
+#Faça um programa que imprima a frase "Hello World!" na tela
+
+#solução:
+
+print("Hello World!")
+
+#a função print deve conter o parentesis () e dentro dele, entre aspas, 
+#LITERALMENTE o conteúdo a ser impresso na tela, caso tenha uma variável e que não esteja entre aspas
+#a função print irá printar o valor da variável
