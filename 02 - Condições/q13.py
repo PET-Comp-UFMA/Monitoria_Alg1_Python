826462a3715bbdb203858a7af567e1911f40d25c
Maikon Keslley <56488687+maikonkc@users.noreply.github.com>
2020-03-09 11:53:07 -0300

    Add hard questions 12 and 13

---
+++ 02 - Condições/q13.py
@@ -0,0 +1,12 @@
+n1,n2,n3 = int(input()),int(input()),int(input())
+
+if n1!=1 and n2!=1 and n3!=1 :
+    print("1")
+if n1!=2 and n2!=2 and n3!=2 :
+    print("2")
+if n1!=3 and n2!=3 and n3!=3 :
+    print("3")
+if n1!=4 and n2!=4 and n3!=4 :
+    print("4")
+if n1!=5 and n2!=5 and n3!=5 :
+    print("5")
