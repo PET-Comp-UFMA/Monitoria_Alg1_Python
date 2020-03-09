944f6d50bc3757265013fe0bf021ca6a86df251e
luisgudias <luisgdias@outlook.com>
2020-03-09 11:17:40 -0300

    Questões 6, 7 e 8 adicionadas

---
+++ 01 - Variáveis/q07.py
@@ -0,0 +1,9 @@
+'''
+Questão 7
+Escreva um programa que leia um número inteiro e exiba como resultado o seu dobro, e em seguida o
+seu triplo, concatenados. '''
+
+numero = int(input("Digite um número inteiro: "))
+dobro = str(numero*2)
+triplo = str(numero*3)
+print(dobro + triplo)
---
+++ 01 - Variáveis/q08.py
@@ -0,0 +1,12 @@
+'''
+Questão 8
+Faça um programa que receba como entrada um número real qualquer, e que imprima em tela um
+número inteiro maior ou igual à entrada, usando a função teto. Caso o número seja negativo, ele deve
+ser tratado como número positivo. Não utilize estruturas IF. '''
+
+import math
+
+numero = float(input("Digite um número real: "))
+numero = abs(numero)
+numero = math.ceil(numero)
+print(numero)
