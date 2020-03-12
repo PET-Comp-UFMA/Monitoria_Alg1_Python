315555057cea12c502b29ed3170a2681a3b45d21
Maikon Keslley <56488687+maikonkc@users.noreply.github.com>
2020-03-12 20:49:34 -0300

    Atualizada e completa

--- 05 - Vetores e matrizes/q04.py
+++ 05 - Vetores e matrizes/q04.py
@@ -1,15 +1,32 @@
-#Questão 4
-#Faça um programa que receba os 9 números de uma matriz 3x3 e calcule e imprima o seu
-#determinante.
-
 matriz = []
-for i in range(0, 3):
-    matriz.append([])
-    for j in range(0, 3):
-        matriz[i].append(int(input("Digite um número: ")))
+for i in range(3):
+    linha=[]
+    for j in range(3):
+        linha.append(int(input()))
+    matriz.append(linha)
 
-print(matriz)
+for i in range(3):
+    for j in range(2):
+        matriz[i].append(matriz[i][j])
 
-#Questão incompleta.
+somaPrim=0
+j=0
+for i in range(3):
+    somaPrim = somaPrim + matriz[i][j]
+    j= j+1
+j=1
+for i in range(3):
+    somaPrim = somaPrim + matriz[i][j]
+    j= j+1
 
+somaSec = 0
+j=4
+for i in range(3):
+    somaSec = somaSec + matriz[i][j]
+    j= j-1
+j=3
+for i in range(3):
+    somaSec = somaSec + matriz[i][j]
+    j= j-1
 
+print("A determinante é: ",somaPrim-somaSec)
