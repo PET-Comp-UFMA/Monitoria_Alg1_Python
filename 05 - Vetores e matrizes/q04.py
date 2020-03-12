89e9b195e0e0c107df563673b83da0b365e979f0
luisgudias <luisgdias@outlook.com>
2020-03-12 08:11:46 -0300

    Questão 4 de Vetores e matrizes adicionada (incompleta)

---
+++ 05 - Vetores e matrizes/q04.py
@@ -0,0 +1,15 @@
+#Questão 4
+#Faça um programa que receba os 9 números de uma matriz 3x3 e calcule e imprima o seu
+#determinante.
+
+matriz = []
+for i in range(0, 3):
+    matriz.append([])
+    for j in range(0, 3):
+        matriz[i].append(int(input("Digite um número: ")))
+
+print(matriz)
+
+#Questão incompleta.
+
+
