a58591092466d0ea0188b7fc6fd85a8fd09ff322
Gabriel Costa <55096273+Promestein@users.noreply.github.com>
2020-03-24 09:41:03 -0300

    Add files via upload

---
+++ 02 - Condições/q20.py
@@ -0,0 +1,23 @@
+"""
+Crie um programa que receba 3 valores, compare-os e printe ao final o número de valores iguais.
+
+Exemplo: 
+        Entrada     Saída
+        1, 2, 3     0
+        2, 3, 2     2
+        5, 5, 5     3
+
+"""
+
+#Solução
+
+contador=0
+num1, num2, num3 = int(input()), int(input()), int(input())
+if num1==num2:
+    contador+=1
+if num2==num3:
+    contador+=1
+if num1==num3:
+    contador+=1
+
+print("A quantidade de números iguais é: " + str(contador))
