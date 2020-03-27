0c94484d4e3c53f2f4e5e1f1254a6ab64eecf878
Gabriel Costa <55096273+Promestein@users.noreply.github.com>
2020-03-27 11:52:15 -0300

    Questões 12-15 adicionadas

---
+++ 05 - Vetores e matrizes/q13.py
@@ -0,0 +1,22 @@
+"""
+
+Crie um programa que receba um elemento (inteiro ou string) e mostre todos
+os índices de ocorrencia desse parâmetro em uma lista em uma lista.
+
+Exemplo
+        Entrada                     Saída
+        [1,2,2,1,4,3], 2            [1,2]
+        ["a","a","b","a","c"],"a"   [0,1,3]
+
+"""
+
+#Solução
+
+v=["a","a","b","a","c"]
+posições=[]
+elemento=input()
+
+for i in range(0,len(v),1):
+    if v[i]==elemento:
+        posições.append(i)
+print(posições)
