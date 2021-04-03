24a7fd56a61725f6f939546be76dc4bc1f0ff881
Gabriel Costa <55096273+Promestein@users.noreply.github.com>
2021-04-03 18:46:55 -0300

    Update q05.py

--- 03 - Laços de repetição/q05.py
+++ 03 - Laços de repetição/q05.py
@@ -1,11 +1,21 @@
-print("Digite um numero")
-numero = int(input())
+"""
+
+Faça um programa que receba um número inteiro e mostre ao 
+final a soma dos numeros pelos quais a entrada é divisível.
+
+Exemplo: Entrada:10
+         Saída: 8
+         Pois o numero 10 pode ser dividido por 1, 2 e 5.
+
+"""
+
+numero = int(input("Digite um numero: "))
 somaDivisao = 0
 cont = 0
 
-for i in range(1,int(numero/2)): 
+for i in range(1,int(numero/2+1)): 
     cont=cont+1
     if numero%cont == 0:   
         somaDivisao = somaDivisao + cont
 
-print(str(somaDivisao))
+print(str(somaDivisao+numero))
