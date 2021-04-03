920b7f7b3a17252c3fa8ae67ca92095aaff61bae
Gabriel Costa <55096273+Promestein@users.noreply.github.com>
2021-04-03 18:59:10 -0300

    Update q07.py

--- 03 - Laços de repetição/q07.py
+++ 03 - Laços de repetição/q07.py
@@ -1,5 +1,34 @@
-for i in range(9):
-    for j in range(9):
+"""
+
+Faça um programa que receba um inteiro positivo e imprima a seguinte sequência
+de asteriscos de acordo com o numero recebido.
+
+Exemplo:  
+
+Entrada: 5  
+Saída:
+*
+* *
+* * *
+* * * *
+* * * * *
+
+Entrada: 9
+saída:
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+* * * * * * *
+* * * * * * * *
+* * * * * * * * *
+
+"""
+linhas = int(input("Digite o numero de linhas do padrão: "))
+for i in range(linhas):
+    for j in range(linhas):
         if i >= j:
             print("* ", end="")#o parâmetro 'end=""' faz com que não haja quebra de linha
         else:   
