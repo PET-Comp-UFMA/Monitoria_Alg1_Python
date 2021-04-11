24f99e3e4c1f028ffe4ec556e452a5c0467d3ab3
Gabriel Costa <55096273+Promestein@users.noreply.github.com>
2021-04-11 16:45:50 -0300

    Exemplo adicionado

--- 05 - Vetores e matrizes/q05.py
+++ 05 - Vetores e matrizes/q05.py
@@ -1,6 +1,17 @@
-#Faça uma função que receba um vetor de tamanho N com a altura de N pessoas na fila de uma padaria
-#e retorna quantas podem ser vistas pelo padeiro. Use a seguinte assinatura: function visiveisFila(n)
+"""
 
+Faça uma função que receba um vetor de tamanho N com a altura de N pessoas na fila de uma padaria
+e retorna quantas podem ser vistas pelo padeiro. Use a seguinte assinatura: function visiveisFila(n).
+Uma pessoa pode ser vista pelo padeiro se não houver nenhuma pessoa mais alta que ela em sua frente,
+para entender melhor observe o exemplo.
+
+Exemplo
+Entrada                     Saída
+[1.53, 1.70, 1.65, 1.80]    3
+3 pessoas podem ser vistas, pois ele pode ver a posição 0, 1 e 3 da lista, mas não pode ver a 2, já
+que ela é mais baixa que a posição 1.
+
+"""
 def visiveisFila(v):
     maisAlto = 0
     contador = 0
